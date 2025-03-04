f4c7cb36727482695b4314607bd0b2552b8e3b5b
Asim Williams <asimwilliams@google.com>
2025-03-04 00:49:01 -0500

    setup express and can run server

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+// We use express to create our servers endpoints, and listen for & respond to requests from the frontend
+import express from "express";
+// We use dotenv so that we can access our environment variables
+import "dotenv/config";
+
+// Create an instance of express
+const requestHandler = express();
+// Storing our port value from the .env file
+const port = process.env.PORT;
+
+
+
+
+
+
+
+// Starting our http server and listening for requests!
+requestHandler.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
